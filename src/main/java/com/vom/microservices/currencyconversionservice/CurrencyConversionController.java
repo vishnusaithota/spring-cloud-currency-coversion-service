e7f63db55140e065b9fb25a6f2f70409982e7b86
tnvrravish <vt7@njit.edu>
2023-04-09 14:20:08 -0400

    added new API for conversion to take response from currency exchange using RestTemplate and mapping it to Currency Conversion object and return it.

---
+++ src/main/java/com/vom/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,29 @@
+package com.vom.microservices.currencyconversionservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.HashMap;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
+                                                          @PathVariable String to,
+                                                          @PathVariable Integer quantity){
+
+        HashMap<String, String> uriVariables =  new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
+                ("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversion.class, uriVariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        return currencyConversion;
+    }
+}
